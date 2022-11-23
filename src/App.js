01387791e61e36882e4b176fb6f611873640204c
Omar Al-Ashi <omar.ashi@hotmail.com>
2022-11-23 23:17:59 +0400

    Setup routing in App.js and use BuildRoutes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+import BuildRoutes from './routing/BuildRoutes'
 import Header from "./components/Header";
 
 function App() {
   return (
+    <BrowserRouter>
       <div className="App">
         <Header/>
+        <BuildRoutes/>
       </div>
+    </BrowserRouter>
   );
 }
 
