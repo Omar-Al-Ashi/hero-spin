096f3f4aafe5d513d6c0d141e23280b5ccef0a69
Omar Al-Ashi <omar.ashi@hotmail.com>
2022-11-24 00:28:43 +0400

    Implement HomePage draft

--- src/containers/HomePage/HomePage.container.js
+++ src/containers/HomePage/HomePage.container.js
@@ -1,10 +1,34 @@
 import React from "react";
 import './HomePage.container.css'
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import {v4 as uuidv4} from 'uuid';
+
 
 const HomePage = () => {
+
+  const pages = [
+    {
+      id: uuidv4,
+      link: '/generate/random',
+      name: 'Random Generator Page'
+    },
+    {
+      id: uuidv4,
+      link: '/generate/select',
+      name: 'Select Your Hero Generator Page'
+    }
+  ]
+
+
   return (
-    <div>
-      Hello from HomePage
+    <div className='pages-selector-grid'>
+      {pages.map(page => (
+        <a href={page.link} className='page-card center'>
+          <div>
+            <p>{page.name}</p>
+          </div>
+        </a>
+      ))}
     </div>
   )
 }
