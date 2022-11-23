a43b1f8a094d2cf33c304d7939262ea10ce2f56e
Omar Al-Ashi <omar.ashi@hotmail.com>
2022-11-23 23:17:25 +0400

    Create BuildRoutes and setup routes and components

---
+++ src/routing/BuildRoutes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import HomePage from '../containers/HomePage/HomePage.container'
+import RandomGeneratorPage from '../containers/Generator/RandomGenerator.container'
+import DetailsPage from '../containers/Details/Details.container'
+import SelectGeneratorPage from "../containers/Generator/SelectGenerator.container";
+
+const BuildRoutes = () => (
+  <main>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/generate/random" exact element={<RandomGeneratorPage/>}/>
+      <Route path="/generate/select" exact element={<SelectGeneratorPage/>}/>
+      <Route path="/details" exact element={<DetailsPage/>}/>
+    </Routes>
+  </main>
+);
+
+export default React.memo(BuildRoutes)
