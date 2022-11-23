22daf5ddc9b4f45d5a8223245f8b53b8bd9d01f7
Omar Al-Ashi <omar.ashi@hotmail.com>
2022-11-23 23:16:43 +0400

    Add Generator container placeholder

---
+++ src/containers/Generator/RandomGenerator.container.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './RandomGenerator.container.css'
+
+const Generator = () => {
+return (
+  <div>
+    This is the Random Generator Page
+  </div>
+)
+}
+
+export default Generator;
---
+++ src/containers/Generator/SelectGenerator.container.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './SelectGenerator.container.css'
+
+const Generator = () => {
+  return (
+    <div>
+      This is the Select Generator Page
+    </div>
+  )
+}
+
+export default Generator;
