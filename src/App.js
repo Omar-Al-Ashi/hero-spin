426368f9f08b4b482d52edc7a3d5640cdee7043f
Omar Al-Ashi <omar.ashi@hotmail.com>
2022-11-25 00:41:45 +0400

    Implement bare bone ErrorHandler ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import {BrowserRouter} from 'react-router-dom';
 import BuildRoutes from './routing/BuildRoutes'
 import Header from "./components/Header";
+import {ErrorBoundary} from 'react-error-boundary'
+import ErrorHandler from './containers/ErrorHandler'
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header/>
-        <BuildRoutes/>
-      </div>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={ErrorHandler}>
+      <BrowserRouter>
+        <div className="App">
+          <Header/>
+          <BuildRoutes/>
+        </div>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
---
+++ src/containers/ErrorHandler/ErrorHandler.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './ErrorHandler.css'
+
+const ErrorHandler = ({error}) => {
+  return (
+    <div>
+      An error has occurred {error}
+    </div>
+  )
+}
+
+export default ErrorHandler;
