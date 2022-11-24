59669440f9002718f1bf5156800531590a9e4513
Omar Al-Ashi <omar.ashi@hotmail.com>
2022-11-24 23:56:52 +0400

    Minor update to Card component to make it more generic

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,31 +1,16 @@
-import React, {useEffect} from 'react';
+import React from 'react';
+import './Card.css'
 import "primeicons/primeicons.css";
 import "primereact/resources/themes/md-light-indigo/theme.css";
 import "primereact/resources/primereact.css";
 import "primeflex/primeflex.css";
-import {Card as PrimeReactCard} from "primereact/card";
 
-const Card = ({headerText, image, subTitle}) => {
-
-  const header = (
-    <img
-      alt="Card"
-      src={image}
-      style={{height: '15rem', width: '20rem', objectFit: 'contain'}}
-      onError={(e) =>
-        (e.target.src = image)
-      }
-    />
-  );
-
+const Card = ({headerText, image}) => {
   return (
-    <PrimeReactCard
-      title={headerText}
-      subTitle={subTitle}
-      header={header}
-      style={{width: "20em", height: '20rem'}}
-    >
-    </PrimeReactCard>
+    <div className="result">
+      <img src={image} alt={image}/>
+      <h3>{headerText}</h3>
+    </div>
   )
 }
 
