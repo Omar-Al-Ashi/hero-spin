8121bb1e0274dd742176742054660d91ed37ac25
Omar Al-Ashi <omar.ashi@hotmail.com>
2022-11-24 23:57:57 +0400

    Implement the basic functionality of the Details container

--- src/containers/Details/Details.container.js
+++ src/containers/Details/Details.container.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useLocation, useNavigate} from 'react-router-dom';
 import './Details.container.css'
 
 const Details = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [movie, setMovie] = useState({})
+
+
+  useEffect(() => {
+    // if the user is coming from a generator page, present the details, else, navigate to homepage
+    location.state ? setMovie(location.state) : navigate('/')
+  })
+
   return (
-    <div>Details Page</div>
+    <>
+      <img src={movie.Poster}/>
+      <h2>Title: {movie.Title}</h2>
+      <p>Type: {movie.Type}</p>
+      <p>Year: {movie.Year}</p>
+    </>
   )
 }
 
