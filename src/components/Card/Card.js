3e340eb65fa3d916a494dac0bb3c90eb52676e62
Omar Al-Ashi <omar.ashi@hotmail.com>
2022-11-25 09:18:41 +0400

    Update Card component to accept clickHandler

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,9 +5,9 @@ import "primereact/resources/themes/md-light-indigo/theme.css";
 import "primereact/resources/primereact.css";
 import "primeflex/primeflex.css";
 
-const Card = ({headerText, image}) => {
+const Card = ({headerText, image, cardClicked}) => {
   return (
-    <div className="result">
+    <div className="result" onClick={cardClicked}>
       <img src={image} alt={image}/>
       <h3>{headerText}</h3>
     </div>
