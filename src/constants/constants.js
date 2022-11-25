bdc17aa12037ea6f4dfd431a20a4e6e3521f6f28
Omar Al-Ashi <omar.ashi@hotmail.com>
2022-11-25 09:18:20 +0400

    Add superheros static json

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,125 @@
 export default {
-  apiKey: 'a2526df0'
+  apiKey: 'a2526df0',
+  superheros: [
+    {
+      "superhero": "Batman",
+      "publisher": "DC Comics",
+      "alter_ego": "Bruce Wayne",
+      "characters": "Bruce Wayne"
+    },
+    {
+      "superhero": "Superman",
+      "publisher": "DC Comics",
+      "alter_ego": "Kal-El",
+      "characters": "Kal-El"
+    },
+    {
+      "superhero": "Flash",
+      "publisher": "DC Comics",
+      "alter_ego": "Jay Garrick",
+      "characters": "Jay Garrick, Barry Allen, Wally West, Bart Allen"
+    },
+    {
+      "superhero": "Green Lantern",
+      "publisher": "DC Comics",
+      "alter_ego": "Alan Scott",
+      "characters": "Alan Scott, Hal Jordan, Guy Gardner, John Stewart, Kyle Raynor, Jade, Sinestro, Simon Baz"
+    },
+    {
+      "superhero": "Green Arrow",
+      "publisher": "DC Comics",
+      "alter_ego": "Oliver Queen",
+      "characters": "Oliver Queen"
+    },
+    {
+      "superhero": "Wonder Woman",
+      "publisher": "DC Comics",
+      "alter_ego": "Princess Diana",
+      "characters": "Princess Diana"
+    },
+    {
+      "superhero": "Martian Manhunter",
+      "publisher": "DC Comics",
+      "alter_ego": "J'onn J'onzz",
+      "characters": "Martian Manhunter"
+    },
+    {
+      "superhero": "Robin/Nightwing",
+      "publisher": "DC Comics",
+      "alter_ego": "Dick Grayson",
+      "characters": "Dick Grayson"
+    },
+    {
+      "superhero": "Blue Beetle",
+      "publisher": "DC Comics",
+      "alter_ego": "Dan Garret",
+      "characters": "Dan Garret, Ted Kord, Jaime Reyes"
+    },
+    {
+      "superhero": "Black Canary",
+      "publisher": "DC Comics",
+      "alter_ego": "Dinah Drake",
+      "characters": "Dinah Drake, Dinah Lance"
+    },
+    {
+      "superhero": "Spider Man",
+      "publisher": "Marvel Comics",
+      "alter_ego": "Peter Parker",
+      "characters": "Peter Parker"
+    },
+    {
+      "superhero": "Captain America",
+      "publisher": "Marvel Comics",
+      "alter_ego": "Steve Rogers",
+      "characters": "Steve Rogers"
+    },
+    {
+      "superhero": "Iron Man",
+      "publisher": "Marvel Comics",
+      "alter_ego": "Tony Stark",
+      "characters": "Tony Stark"
+    },
+    {
+      "superhero": "Thor",
+      "publisher": "Marvel Comics",
+      "alter_ego": "Thor Odinson",
+      "characters": "Thor Odinson"
+    },
+    {
+      "superhero": "Hulk",
+      "publisher": "Marvel Comics",
+      "alter_ego": "Bruce Banner",
+      "characters": "Bruce Banner"
+    },
+    {
+      "superhero": "Wolverine",
+      "publisher": "Marvel Comics",
+      "alter_ego": "James Howlett",
+      "characters": "James Howlett"
+    },
+    {
+      "superhero": "Daredevil",
+      "publisher": "Marvel Comics",
+      "alter_ego": "Matthew Michael Murdock",
+      "characters": "Matthew Michael Murdock"
+    },
+    {
+      "superhero": "Hawkeye",
+      "publisher": "Marvel Comics",
+      "alter_ego": "Clinton Francis Barton",
+      "characters": "Clinton Francis Barton"
+    },
+    {
+      "superhero": "Cyclops",
+      "publisher": "Marvel Comics",
+      "alter_ego": "Scott Summers",
+      "characters": "Scott Summers"
+    },
+    {
+      "superhero": "Silver Surfer",
+      "publisher": "Marvel Comics",
+      "alter_ego": "Norrin Radd",
+      "characters": "Norrin Radd"
+    }
+  ]
 }
